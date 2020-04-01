05292bcf0ee15da19ea408d604f0d7c5b7b9ce7a
chamodlakmal <chamodlakmal97@gmail.com>
2020-04-01 22:36:36 +0530

    part 8 added

--- EventHandling/app/src/main/java/com/chamiviews/eventhandling/MainActivity.java
+++ EventHandling/app/src/main/java/com/chamiviews/eventhandling/MainActivity.java
@@ -3,12 +3,34 @@ package com.chamiviews.eventhandling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.txtView);
+        button=findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hi");
+            }
+        });
+
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                textView.setText("ChamiViews");
+                return true;
+            }
+        });
+
     }
+
 }
