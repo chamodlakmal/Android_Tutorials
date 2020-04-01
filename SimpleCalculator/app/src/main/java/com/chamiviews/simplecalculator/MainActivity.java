697e5126ca3c2b4c8aa4f71c2e4b536e33e3d19e
chamodlakmal <chamodlakmal97@gmail.com>
2020-04-01 20:18:32 +0530

    part 7

--- SimpleCalculator/app/src/main/java/com/chamiviews/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/chamiviews/simplecalculator/MainActivity.java
@@ -6,23 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1;
-    EditText firstNumber;
+    EditText firstNumber,secondNumber;
+    TextView answer;
+    String num1,num2;
+    Double number1,number2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1=findViewById(R.id.btnAdd);
         firstNumber=findViewById(R.id.txt1);
-
+        secondNumber=findViewById(R.id.txt2);
+        answer=findViewById(R.id.txtAnswer);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("ChamiViews");
-                System.out.println(firstNumber.getText());
+               num1=firstNumber.getText().toString();
+               num2=secondNumber.getText().toString();
+
+               try{
+                   number1=Double.parseDouble(num1);
+                   number2=Double.parseDouble(num2);
+                   answer.setText((number1+number2)+"");
+               }catch(Exception e){
+                   answer.setText("Error");
+               }
+
             }
+
+
+
+
         });
 
     }
