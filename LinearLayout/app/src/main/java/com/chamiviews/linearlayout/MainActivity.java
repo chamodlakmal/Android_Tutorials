c986960bbfd1fa2f291903d9f25a8e6001555749
chamod_lab <chamodlakmal97@gmail.com>
2020-08-20 21:18:14 +0530

    updated

--- LinearLayout/app/src/main/java/com/chamiviews/linearlayout/MainActivity.java
+++ LinearLayout/app/src/main/java/com/chamiviews/linearlayout/MainActivity.java
@@ -2,13 +2,24 @@ package com.chamiviews.linearlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1=findViewById(R.id.btn1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
